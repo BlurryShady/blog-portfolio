578d815bede8c4e97516e348989ba66c26dcac97
BlurryShady <blurryshady@hotmail.com>
2025-12-07 00:13:09 +0300

    Add password Reset urls

--- personalblog/urls.py
+++ personalblog/urls.py
@@ -8,13 +8,12 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
+    1. Add an import:  from another_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 
 # personalblog/urls.py
 from django.contrib import admin
@@ -24,9 +23,7 @@ from django.views.generic import TemplateView
 from blog.sitemaps import PostSitemap, CategorySitemap, StaticSitemap
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-
+from django.contrib.auth import views as auth_views
 
 sitemaps = {
     "posts": PostSitemap,
@@ -36,7 +33,30 @@ sitemaps = {
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls", namespace= "blog")),
+    path("", include("blog.urls", namespace="blog")),
+
+    # Password reset flow (Django auth)
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
     path(
         "robots.txt",
@@ -45,7 +65,5 @@ urlpatterns = [
     ),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
