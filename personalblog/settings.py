4a64a608a1772ffb4568743cc69a6cd447a603d6
BlurryShady <blurryshady@hotmail.com>
2025-12-06 23:33:55 +0300

    Email integration for forgot password

--- personalblog/settings.py
+++ personalblog/settings.py
@@ -185,23 +185,15 @@ def env_bool(name: str, default: str = "false") -> bool:
     }
 
 
-# Email (console locally, SMTP in production)
-EMAIL_BACKEND = os.getenv(
-    "EMAIL_BACKEND",
-    "django.core.mail.backends.smtp.EmailBackend"
-    if os.getenv("EMAIL_HOST")
-    else "django.core.mail.backends.console.EmailBackend",
-)
-EMAIL_HOST = os.getenv("EMAIL_HOST", "")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", "true")
-EMAIL_USE_SSL = env_bool("EMAIL_USE_SSL", "false")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv(
-    "DEFAULT_FROM_EMAIL",
-    "Blurry Shady Blog <noreply@blog.blurryshady.me>",
-)
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
 
 
 # --- Production hardening (enable on deploy) ---
